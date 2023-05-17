2ba473e12f48a100d8aa9f21da17d7509f54e6ea
aeong <kyy2374@gmail.com>
2023-05-18 03:21:49 +0900

    특정 위치 마커 표시하기

--- app/src/main/java/org/techtown/mynavermap/MainActivity.java
+++ app/src/main/java/org/techtown/mynavermap/MainActivity.java
@@ -1,14 +1,84 @@
 package org.techtown.mynavermap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS = new String[]{
+            "Manifest.permission.ACCESS_FINE_LOCATION",
+            "Manifest.permission.ACCESS_COARSE_LOCATION"
+    };
+
+    private FusedLocationSource mLocationSource;
+    private NaverMap mNaverMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 지도 객체 생성
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        // getMapAsync를 호출하여 비동기로 onMapReady 콜백 메서드 호출
+        // onMapReady에서 NaverMap 객체를 받음
+        mapFragment.getMapAsync(this);
+
+        // 위치를 반환하는 구현체인 FusedLocationSource 생성
+        mLocationSource =
+                new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        Log.d( TAG, "onMapReady");
+
+        // 지도상에 마커 표시
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
+
+        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        mNaverMap = naverMap;
+        mNaverMap.setLocationSource(mLocationSource);
+
+        // 권한확인. 결과는 onRequestPermissionsResult 콜백 매서드 호출
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // request code와 권한획득 여부 확인
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
     }
 }
